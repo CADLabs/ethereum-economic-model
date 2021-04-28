730ea8e1d68a394fec289ea8ec9a228388f72ad8
BenSchZA <ben@bitsofether.com>
2021-04-28 13:56:34 +0200

    Add logging

--- experiments/run.py
+++ experiments/run.py
@@ -1,14 +1,27 @@
 import pandas as pd
+import logging
+from datetime import datetime
 
 from experiments.default import experiment
 
 
+# Configure logging framework
+# e.g. Use logging.debug(...) to log to log file
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(filename=f'logs/experiment-{datetime.now()}.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
 def run(experiment=experiment):
-  print("Running experiment")
+  logging.info("Running experiment")
   experiment.run()
-  print("Experiment complete")
+  logging.info("Experiment complete")
 
   return experiment.results, experiment.exceptions
+
 
 if __name__ == '__main__':
   results, _exceptions = run()
