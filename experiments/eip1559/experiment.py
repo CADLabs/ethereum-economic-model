73a47bac590f563e05ab9acb59dcd64ea7f672b4
BenSchZA <ben@bitsofether.com>
2021-04-29 21:47:17 +0200

    Update experiment

--- experiments/eip1559/experiment.py
+++ experiments/eip1559/experiment.py
@@ -5,8 +5,16 @@ from experiments.default import experiment
 
 parameter_overrides = {
     # Sweep of EIP1559 disabled and enabled
-    "eip1559_basefee_process": [lambda _run, _timestep: 0, lambda _run, _timestep: 100, lambda _run, _timestep: 70],  # Gwei per gas
-    "eip1559_tip_process": [lambda _run, _timestep: 0, lambda _run, _timestep: 1, lambda _run, _timestep: 30],  # Gwei per gas
+    "eip1559_basefee_process": [
+        lambda _run, _timestep: 0,
+        lambda _run, _timestep: 100,
+        lambda _run, _timestep: 70
+    ],  # Gwei per gas
+    "eip1559_tip_process": [
+        lambda _run, _timestep: 0,
+        lambda _run, _timestep: 1,
+        lambda _run, _timestep: 30
+    ],  # Gwei per gas
 }
 
 # Override default experiment parameters
