55724e0e375bce326070433f4c643a7ab68f1eee
BenSchZA <ben@bitsofether.com>
2021-06-09 14:44:20 +0200

    Add docs

--- experiments/base.py
+++ experiments/base.py
@@ -10,7 +10,11 @@ from model.state_update_blocks import state_update_blocks
 from model.simulation_configuration import TIMESTEPS, DELTA_TIME, MONTE_CARLO_RUNS
 
 
+# Create Model
 model = Model(params=parameters, initial_state=initial_state, state_update_blocks=state_update_blocks)
+# Create Model Simulation
 simulation = Simulation(model=model, timesteps=TIMESTEPS, runs=MONTE_CARLO_RUNS)
+# Create Experiment of single Simulation
 experiment = Experiment([simulation])
+# Configure Experiment engine
 experiment.engine = Engine(drop_substeps=True)
