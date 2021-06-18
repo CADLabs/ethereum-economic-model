7c4b2ef4432d1d8d7da8aabd84293a6cb8f90b49
BenSchZA <ben@bitsofether.com>
2021-06-18 15:15:07 +0200

    Add timing to run.py

--- experiments/run.py
+++ experiments/run.py
@@ -2,6 +2,7 @@ import pandas as pd
 import logging
 import sys
 from datetime import datetime
+import time
 
 from experiments.default_experiment import experiment
 from experiments.post_processing import post_process
@@ -17,16 +18,19 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-
+from experiments.templates.state_space_analysis import experiment
 def run(experiment=experiment):
   logging.info("Running experiment")
+  start_time = time.time()
   experiment.run()
-  logging.info("Experiment complete")
+  experiment_duration = time.time() - start_time
+  logging.info(f"Experiment complete in {experiment_duration} seconds")
 
   logging.info("Post-processing results")
   df = pd.DataFrame(experiment.results)
   df = post_process(df)
-  logging.info("Post-processing complete")
+  post_processing_duration = time.time() - start_time - experiment_duration
+  logging.info(f"Post-processing complete in {post_processing_duration} seconds")
 
   return df, experiment.exceptions
 
