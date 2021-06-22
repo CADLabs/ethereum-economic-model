a71b6e5bd7e40e3f811aa57cc162abda97494116
BenSchZA <ben@bitsofether.com>
2021-06-22 14:56:56 +0200

    Add get_sim_hash method

--- experiments/utils.py
+++ experiments/utils.py
@@ -13,3 +13,20 @@ def generate_cartesion_product(sweeps):
     cartesian_product = list(itertools.product(*sweeps.values()))
     params = {key: [x[i] for x in cartesian_product] for i, key in enumerate(sweeps.keys())}
     return params
+
+
+def get_simulation_hash(sim):
+    model = sim.model
+    timesteps = sim.timesteps
+    runs = sim.runs
+
+    filtered_initial_state = filter(lambda x: isinstance(x, collections.Hashable), model.initial_state.items())
+    initial_state_hashable = frozenset({key: value for key, value in filtered_initial_state})
+
+    param_keys_hashable = tuple(model.params.keys())
+    param_values = [value for param_list in model.params.values() for value in param_list]
+    param_values_hashable = tuple(filter(lambda x: isinstance(x, collections.Hashable), param_values))
+
+    to_hash = (initial_state_hashable, param_keys_hashable, param_values_hashable, timesteps, runs)
+
+    return hash(to_hash)
