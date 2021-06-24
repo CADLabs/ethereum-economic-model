ff6142b9e75909b498b2d8449c8dc69c45d3fa59
rogervs <rogervs@protonmail.com>
2021-06-24 17:27:36 +0100

    Define cadlabs themes

--- experiments/notebooks/plotly_theme.py
+++ experiments/notebooks/plotly_theme.py
@@ -3,19 +3,206 @@ import plotly.io as pio
 import sys
 
 
-default_template = go.layout.Template()
+pio.templates["cadlabs"] = go.layout.Template(
+    layout_font={'color': '#2a3f5f'},   
+    layout_plot_bgcolor='white',
+    layout_paper_bgcolor='white',
+    layout_colorway=['#FC1CBF','#3283FE','#AA0DFE','#F6222E','#1CBE4F','#FA0087','#B10DA1','#1C8356','#FE00FA','#782AB6'],
+    layout_xaxis={
+            'automargin': True,
+            'showgrid': True,
+            'gridcolor': '#EBF0F8',
+            'linecolor': '#EBF0F8',
+            'ticks': '',
+            'title': {'standoff': 15},
+            'zerolinecolor': '#EBF0F8',
+            'zerolinewidth': 2,
+    },
+    layout_title = {'x': 0.05},
+    layout_yaxis={
+            'automargin': True,
+            'showgrid' : True,
+            'gridcolor': '#EBF0F8',
+            'linecolor': '#EBF0F8',
+            'ticks': '',
+            'title': {'standoff': 15},
+            'zerolinecolor': '#EBF0F8',
+            'zerolinewidth': 2
+    },
+    layout_scene={
+        'xaxis':{
+                'backgroundcolor': 'white',
+                'gridcolor': '#DFE8F3',
+                'gridwidth': 2,
+                'linecolor': '#EBF0F8',
+                'showbackground': True,
+                'ticks': '',
+                'zerolinecolor': '#EBF0F8'
+             },
+             'yaxis':{
+                'backgroundcolor': 'white',
+                'gridcolor': '#DFE8F3',
+                'gridwidth': 2,
+                'linecolor': '#EBF0F8',
+                'showbackground': True,
+                'ticks': '',
+                'zerolinecolor': '#EBF0F8'
+             },
+            'zaxis':{
+                'backgroundcolor': 'white',
+                'gridcolor': '#DFE8F3',
+                'gridwidth': 2,
+                'linecolor': '#EBF0F8',
+                'showbackground': True,
+                'ticks': '',
+                'zerolinecolor': '#EBF0F8'}
+    },
+    layout_shapedefaults={
+            'line': {
+                'color': '#2a3f5f'
+            }
+    },
+    layout_ternary={
+            'aaxis': {
+                'gridcolor': '#DFE8F3', 
+                'linecolor': '#A2B1C6', 
+                'ticks': ''
+            },
+            'baxis': {
+                'gridcolor': '#DFE8F3', 
+                'linecolor': '#A2B1C6', 
+                'ticks': ''
+            },
+            'bgcolor': 'white',
+            'caxis': {
+                'gridcolor': '#DFE8F3', 
+                'linecolor': '#A2B1C6', 
+                'ticks': ''
+            }
+    },
+    
+    layout_annotationdefaults= {
+            'arrowcolor': '#2a3f5f', 
+            'arrowhead': 0, 
+            'arrowwidth': 1
+            },
+    layout_autotypenumbers='strict',
+    layout_coloraxis={
+            'colorbar': {
+                'outlinewidth': 0, 
+                'ticks': ''
+            }
+    },
+    layout_colorscale={
+             'sequential': [
+                 [0,'#F76746'],
+                 [0.33,'#D03DD2'],
+                 [0.66,'#30B4F1'],
+                 [1.0,'#1AD5A0']
+             ],
+     },
+    layout_hoverlabel={
+        'align': 'left'
+    },
+    layout_hovermode='closest',
+    layout_mapbox={
+        'style': 'light'
+    },
+    layout_geo={
+        'bgcolor': 'white',
+        'lakecolor': 'white',
+        'landcolor': 'white',
+        'showlakes': True,
+        'showland': True,
+        'subunitcolor': '#C8D4E3'},
+    layout_polar={
+        'angularaxis': {
+            'gridcolor': '#EBF0F8', 
+            'linecolor': '#EBF0F8', 
+            'ticks': ''
+        },
+        'bgcolor': 'white',
+        'radialaxis': {
+            'gridcolor': '#EBF0F8', 
+            'linecolor': '#EBF0F8', 
+            'ticks': ''}
+    },
+    
+)
 
-default_template.layout.height = 800  # pixels
 
-default_template.layout.title = dict(
-    x=0.5
+pio.templates["cadlabs_dark"] = go.layout.Template(
+    layout_font={'color': '#f2f5fa'},   
+    layout_plot_bgcolor='rgb(17,17,17)',
+    layout_paper_bgcolor='rgb(17,17,17)',
+    layout_colorway=['#FC1CBF','#3283FE','#AA0DFE','#F6222E','#1CBE4F','#FA0087','#B10DA1','#1C8356','#FE00FA','#782AB6'],
+    layout_xaxis={
+            'automargin': True,
+            'showgrid': True,
+            'gridcolor': '#283442',
+            'linecolor': '#506784',
+            'ticks': '',
+            'title': {'standoff': 15},
+            'zerolinecolor': '#283442',
+            'zerolinewidth': 2,
+    },
+    layout_title = {'x': 0.05},
+    layout_yaxis={
+            'automargin': True,
+            'showgrid' : True,
+            'gridcolor': '#283442',
+            'linecolor': '#506784',
+            'ticks': '',
+            'title': {'standoff': 15},
+            'zerolinecolor': '#283442',
+            'zerolinewidth': 2},
+     layout_colorscale={
+             'sequential': [
+                 [0,'#F76746'],
+                 [0.33,'#D03DD2'],
+                 [0.66,'#30B4F1'],
+                 [1.0,'#1AD5A0']
+             ],
+     }
 )
-default_template.layout.xaxis = dict(
-    showgrid=True
+
+pio.templates["cadlabs_frontend"] = go.layout.Template(
+    layout_font={'color': '#ffffff'},
+    layout_plot_bgcolor='#272838',
+    layout_paper_bgcolor='#272838',
+    layout_colorway=['#FC1CBF','#3283FE','#AA0DFE','#F6222E','#1CBE4F','#FA0087','#B10DA1','#1C8356','#FE00FA','#782AB6'],
+    layout_xaxis={
+            'automargin': True,
+            'showgrid': True,
+            'gridcolor': '#3C3D56',
+            'linecolor': '#3C3D56',
+            'ticks': '',
+            'title': {'standoff': 15},
+            'zerolinecolor': '#272838',
+            'zerolinewidth': 3,
+    },
+    layout_title = {'x': 0.05},
+    layout_yaxis={
+            'automargin': True,
+            'showgrid' : True,
+            'gridcolor': '#3C3D56',
+            'linecolor': '#3C3D56',
+            'ticks': '',
+            'title': {'standoff': 15},
+            'zerolinecolor': '#3C3D56',
+            'zerolinewidth': 3},
+     layout_colorscale={
+             'sequential': [
+                 [0,'#F76746'],
+                 [0.33,'#D03DD2'],
+                 [0.66,'#30B4F1'],
+                 [1.0,'#1AD5A0']
+             ],
+     }
 )
-default_template.layout.yaxis = dict(
-    showgrid=True
-)
+
+cadlabs_colors = [[0,'#F76746'],[0.33,'#D03DD2'],[0.66,'#30B4F1'],[1.0,'#1AD5A0']]
+pio.templates.default = "cadlabs"
 
 # Legend at bottom of chart
 # default_template.layout.legend = dict(
@@ -29,6 +216,4 @@ default_template.layout.yaxis = dict(
 #     x=0.5,
 # )
 
-pio.templates["default"] = default_template
-# Other options: "presentation", "simple_white"
-pio.templates.default = "plotly_white+default"
+
