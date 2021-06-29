7e6c9bcab032b28840e9b3cceff85de44678852c
BenSchZA <ben@bitsofether.com>
2021-06-29 10:59:06 +0200

    Reformat run.py

--- experiments/run.py
+++ experiments/run.py
@@ -7,7 +7,6 @@ import time
 from experiments.default_experiment import experiment
 from experiments.post_processing import post_process
 
-
 # Configure logging framework
 # e.g. Use logging.debug(...) to log to log file
 logger = logging.getLogger()
@@ -18,27 +17,33 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+
 def run(executable=experiment):
-  logging.info("Running experiment")
-  start_time = time.time()
-  executable.run()
-  experiment_duration = time.time() - start_time
-  logging.info(f"Experiment complete in {experiment_duration} seconds")
+    logging.info("Running experiment")
+    start_time = time.time()
 
-  logging.info("Post-processing results")
-  df = pd.DataFrame(executable.results)
-  parameters = {}
-  try:
-    parameters = executable.simulations[0].model.params
-  except:
-    parameters = executable.model.params
-  df = post_process(df, parameters=parameters)
-  post_processing_duration = time.time() - start_time - experiment_duration
-  logging.info(f"Post-processing complete in {post_processing_duration} seconds")
+    executable.run()
 
-  return df, executable.exceptions
+    experiment_duration = time.time() - start_time
+    logging.info(f"Experiment complete in {experiment_duration} seconds")
+
+    logging.info("Post-processing results")
+
+    df = pd.DataFrame(executable.results)
+
+    try:
+        parameters = executable.simulations[0].model.params
+    except:
+        parameters = executable.model.params
+
+    df = post_process(df, parameters=parameters)
+
+    post_processing_duration = time.time() - start_time - experiment_duration
+    logging.info(f"Post-processing complete in {post_processing_duration} seconds")
+
+    return df, executable.exceptions
 
 
 if __name__ == '__main__':
-  df, _exceptions = run()
-  print(df)
+    df, _exceptions = run()
+    print(df)
