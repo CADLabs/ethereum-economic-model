f55abeeb7491cf362ec928a59bdc34fc091db559
BenSchZA <ben@bitsofether.com>
2021-07-02 10:37:07 +0200

    Update API defaults

--- data/api/beaconchain.py
+++ data/api/beaconchain.py
@@ -1,5 +1,6 @@
 import requests
 import diskcache
+import logging
 
 from model.types import Gwei
 
@@ -9,14 +10,22 @@ cache = diskcache.Cache('.beaconchain_api.cache')
 
 @cache.memoize(expire=(6 * 60 * 60))  # cached for 6 hours
 def get_epoch_data(epoch="latest"):
-    return requests.get(f"https://beaconcha.in/api/v1/epoch/{epoch}", headers={"accept":"application/json"}).json()["data"]
+    try:
+        req = requests.get(f"https://beaconcha.in/api/v1/epoch/{epoch}", headers={"accept":"application/json"})
+        req.raise_for_status()
+        return req.json()["data"]
+    except requests.exceptions.HTTPError as err:
+        logging.error(err)
+        return {}
 
 
 def get_total_validator_balance(default=None) -> Gwei:
-    result = int(get_epoch_data()["totalvalidatorbalance"])
-    return result if result else default
+    data = get_epoch_data()
+    result = int(data.get("totalvalidatorbalance", default))
+    return result
 
 
 def get_validators_count(default=None) -> int:
-    result = int(get_epoch_data()["validatorscount"])
-    return result if result else default
+    data = get_epoch_data()
+    result = int(data.get("validatorscount", default))
+    return result
--- data/api/etherscan.py
+++ data/api/etherscan.py
@@ -1,5 +1,6 @@
 import requests
 import diskcache
+import logging
 
 from model.types import Wei
 
@@ -9,5 +10,10 @@ cache = diskcache.Cache('.etherscan_api.cache')
 
 @cache.memoize(expire=(6 * 60 * 60))  # cached for 6 hours
 def get_eth_supply(default=None) -> Wei:
-    result = int(requests.get("https://api.etherscan.io/api?module=stats&action=ethsupply", headers={"accept": "application/json"}).json()["result"])
-    return result if result else default
+    try:
+        req = requests.get("https://api.etherscan.io/api?module=stats&action=ethsupply", headers={"accept": "application/json"})
+        req.raise_for_status()
+        return int(req.json()["result"])
+    except requests.exceptions.HTTPError as err:
+        logging.error(err)
+        return default
