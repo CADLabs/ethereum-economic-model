2d8ea00c322de961da73086586c4320dbf6d85a7
BenSchZA <BenSchZA@users.noreply.github.com>
2021-07-08 22:19:46 +0200

    Update API

--- data/api/etherscan.py
+++ data/api/etherscan.py
@@ -13,7 +13,13 @@ def get_eth_supply(default=None) -> Wei:
     try:
         req = requests.get("https://api.etherscan.io/api?module=stats&action=ethsupply", headers={"accept": "application/json"})
         req.raise_for_status()
-        return int(req.json()["result"])
+        # Etherscan returns a JSON object with "status" 0 for failure,
+        # "status" key does not exist for normal response!
+        # Normal HTTP status is ignored.
+        if not int(req.json().get("status", 1)):
+            raise requests.exceptions.HTTPError
+        else:
+            return int(req.json()["result"])
     except requests.exceptions.HTTPError as err:
         logging.error(err)
         return default
